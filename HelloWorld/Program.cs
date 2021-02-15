a03914d3d9b10ec879339b45d45c362dcda18282
WesleyEspinoza <Wesleyespinoza1021@gmail.com>
2021-02-15 17:56:06 -0500

    added first HelloWorld Program

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = Console.ReadLine();
+            Console.WriteLine($"Hello! {name}");
+        }
+    }
+}
